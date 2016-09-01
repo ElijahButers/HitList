cafea9690ca38305b45c85debcdb2d8f0a96fd83
ElijahButers <Elijahbuters@gmail.com>
2016-09-01 17:29:49 +0300

    Added code to viewDidLoad() in ViewController that sets a title and register the UITableViewCell class with the table view.

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        title = "\"The List\""
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
     override func didReceiveMemoryWarning() {
