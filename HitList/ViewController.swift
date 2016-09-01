2c03fd210daf277ca060511bbabd3ded7e2a8296
ElijahButers <Elijahbuters@gmail.com>
2016-09-02 00:06:59 +0300

    Updated code in tableView(numberOfRowsInSection) and tableView(cellForRowAt indexPath) where we match cells with the corresponding NSManagedObject.

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -32,13 +32,14 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return names.count
+        return people.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
-        cell!.textLabel!.text = names[indexPath.row]
+        let person = people[indexPath.row]
+        cell!.textLabel!.text = person.value(forKey: "name") as? String
         
         return cell!
     }
